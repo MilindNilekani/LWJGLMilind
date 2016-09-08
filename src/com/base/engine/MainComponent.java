30a48b98900ebcdd852f07a17ef1d1d824224968
mnilekan@andrew.cmu.edu <mnilekan@andrew.cmu.edu>
2016-09-07 21:38:20 -0400

    First commit

---
+++ src/com/base/engine/MainComponent.java
@@ -0,0 +1,120 @@
+package com.base.engine;
+
+public class MainComponent 
+{	
+	public static final int HEIGHT=800;
+	public static final int WIDTH=600;
+	public static final String TITLE="3D Java Game Engine";
+	public static final double FRAME_CAP=5000.0;
+	
+	private Game game;
+	
+	private boolean isRunning;
+	
+	public MainComponent()
+	{
+		isRunning=false;
+		game=new Game();
+	}
+	
+	public void start()
+	{
+		if(isRunning)
+			return;
+		
+		run();
+	}
+	
+	public void stop()
+	{
+		if(!isRunning)
+			return;
+		
+		isRunning=false;
+	}
+	
+	private void run()
+	{
+		isRunning=true;
+		
+		int frames=0;
+		long frameCounter=0;
+		final double frameTime=1.0/FRAME_CAP;
+		
+		long lastTime=Time.getTime();
+		double unprocessedTime=0;
+		
+		while(!Window.isCloseRequested())
+		{
+			boolean render=false;
+			
+			long startTime=Time.getTime();
+			long passedTime=startTime-lastTime;
+			lastTime=startTime;
+			
+			unprocessedTime+=passedTime/(double)Time.SECOND;
+			frameCounter+=passedTime;
+			
+			while(unprocessedTime>frameTime)
+			{
+				render=true;
+				unprocessedTime-=frameTime;
+				if(Window.isCloseRequested())
+				{
+					stop();
+				}
+				
+				Time.setDelta(frameTime);
+				
+				game.input();
+				game.update();
+				
+				if(frameCounter>=Time.SECOND)
+				{
+					System.out.println(frames);
+					frames=0;
+					frameCounter=0;
+				}
+			}
+		
+			
+			if(render)
+			{
+				render();
+				frames++;
+			}
+			else
+			{
+				try 
+				{
+					Thread.sleep(1);
+				} 
+				catch (InterruptedException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		clear();
+	}
+	
+	private void render()
+	{
+		//game.render();
+		Window.render();
+	}
+	
+	private void clear()
+	{
+		Window.destroy();
+	}
+	
+	public static void main(String[] arg)
+	{
+		Window.createWindow(HEIGHT, WIDTH, TITLE);
+		
+		MainComponent game=new MainComponent();
+		
+		game.start();
+	}
+}
---
+++ src/com/base/engine/Window.java
@@ -0,0 +1,52 @@
+package com.base.engine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Window 
+{
+	public static void createWindow(int height, int width, String title)
+	{
+		Display.setTitle(title);
+		try 
+		{
+			Display.setDisplayMode(new DisplayMode(width,height));
+			Display.create();
+		} 
+		catch (LWJGLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void render()
+	{
+		Display.update();
+	}
+	
+	public static void destroy()
+	{
+		Display.destroy();
+	}
+	
+	public static boolean isCloseRequested()
+	{
+		return Display.isCloseRequested();
+	}
+	
+	public static int getWidth()
+	{
+		return Display.getWidth();
+	}
+	
+	public static int getHeight()
+	{
+		return Display.getHeight();
+	}
+	
+	public static String getTitle()
+	{
+		return Display.getTitle();
+	}
+}
